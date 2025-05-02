0dcbc4a7953a0558b3db72227214399d5b601ef0
iprinceyadav <yadav27prince12@gmail.com>
2025-05-02 22:53:02 +0530

    updated code

--- helper.py
+++ helper.py
@@ -1,5 +1,15 @@
 def fetch_stats(selected_user,df):
-    if selected_user == 'Overall':
-        return df.shape[0]
-    else:
-        return df[df['user']==selected_user].shape[0]
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+
+    num_messages=df.shape[0]
+    words=[]
+    for message in df['message']:
+        words.extend(message.split())
+    
+    num_media_messages=df[df['message']=='<Media omitted>\n'].shape[0]
+
+    return num_messages,len(words),num_media_messages
+    
